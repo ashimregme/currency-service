16f07acaf854135bd70ce74a2c97d329f56aec29
Personal <Personal@Ashims-Macbook-Pro.local>
2022-06-23 22:43:22 +0545

    refactor: removed unnecessary getById method

--- db-service/src/main/java/pl/assecods/dbservice/repository/ExchangeRateRepository.java
+++ db-service/src/main/java/pl/assecods/dbservice/repository/ExchangeRateRepository.java
@@ -3,11 +3,7 @@ package pl.assecods.dbservice.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.assecods.dbservice.domain.ExchangeRate;
-import pl.assecods.dbservice.dto.ExchangeRateResponse;
-
-import java.util.Optional;
 
 @Repository
 public interface ExchangeRateRepository extends CrudRepository<ExchangeRate, String> {
-    Optional<ExchangeRateResponse> getById(String id);
 }
